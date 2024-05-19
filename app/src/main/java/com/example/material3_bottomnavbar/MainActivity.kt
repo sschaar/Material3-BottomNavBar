9f4a4a98b2e08b64b5b29fd5e6fd9c39b452298b
Stefan Schaar <stefan.schaar@tuta.io>
2024-05-19 10:52:10 +0200

    added material3bottomnavbar example

--- app/src/main/java/com/example/material3_bottomnavbar/MainActivity.kt
+++ app/src/main/java/com/example/material3_bottomnavbar/MainActivity.kt
@@ -1,29 +1,145 @@
 package com.example.material3_bottomnavbar
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.material3_bottomnavbar.ui.theme.Material3BottomNavBarTheme
 
+object Destinations {
+    const val MY_LIST_ROUTE = "MyList"
+    const val HOME_ROUTE = "Home"
+    const val SETTINGS_ROUTE = "Settings"
+}
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val onItemClick: () -> Unit
+)
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+
+            val navController = rememberNavController()
+            NavHost(navController, startDestination = Destinations.HOME_ROUTE) {
+                composable(Destinations.MY_LIST_ROUTE) {
+                    MyList()
+                }
+                composable(Destinations.HOME_ROUTE) {
+                    Home()
+                }
+                composable(Destinations.SETTINGS_ROUTE) {
+                    Settings()
+                }
+            }
+
             Material3BottomNavBarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val items = listOf(
+                    BottomNavigationItem(
+                        title = "MyList",
+                        selectedIcon = Icons.Filled.List,
+                        unselectedIcon = Icons.Outlined.List,
+                        onItemClick = {
+                            navController.navigate(Destinations.MY_LIST_ROUTE)
+                        }
+                    ),
+                    BottomNavigationItem(
+                        title = "Home",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home,
+                        onItemClick = {
+                            navController.navigate(Destinations.HOME_ROUTE)
+                        }
+                    ),
+                    BottomNavigationItem(
+                        title = "Settings",
+                        selectedIcon = Icons.Filled.Settings,
+                        unselectedIcon = Icons.Outlined.Settings,
+                        onItemClick = {
+                            navController.navigate(Destinations.SETTINGS_ROUTE)
+                        }
                     )
+                )
+                var selectedItemIndex: Int by rememberSaveable {
+                    mutableIntStateOf(1)
+                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        bottomBar = {
+                            NavigationBar {
+                                items.forEachIndexed { index, item ->
+                                    NavigationBarItem(
+                                        selected = selectedItemIndex == index,
+                                        onClick = {
+                                            selectedItemIndex = index
+                                            when (index) {
+                                                0 -> navController.navigate(Destinations.MY_LIST_ROUTE)
+                                                1 -> navController.navigate(Destinations.HOME_ROUTE)
+                                                2 -> navController.navigate(Destinations.SETTINGS_ROUTE)
+                                            }
+                                        },
+                                        label = {
+                                            Text(text = item.title)
+                                        },
+                                        icon = {
+                                            Icon(
+                                                imageVector = if (index == selectedItemIndex) {
+                                                    item.selectedIcon
+                                                } else item.unselectedIcon,
+                                                contentDescription = item.title
+                                            )
+
+                                        })
+                                }
+                            }
+                        }
+                    ) { innerPadding ->
+                        Surface(modifier = Modifier.fillMaxSize()) {
+                            when (selectedItemIndex) {
+                                0 -> MyList()
+                                1 -> Home()
+                                2 -> Settings()
+                            }
+                        }
+                    }
+
                 }
             }
         }
@@ -31,17 +147,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Home() {
+    Log.d("Home", "Home composable loaded")
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Home")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Material3BottomNavBarTheme {
-        Greeting("Android")
+fun MyList() {
+    Log.d("MyList", "MyList composable loaded")
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "MyList")
+        }
+    }
+}
+
+@Composable
+fun Settings() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Settings")
+        }
     }
 }
